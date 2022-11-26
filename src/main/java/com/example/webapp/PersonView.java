ceca09946f3312b9072be80d3a02bae31d78c188
alejandro-du <alejandro.d.a@gmail.com>
2022-11-26 15:17:54 +0200

    Simplify view and add NoSQL port forwarding

--- src/main/java/com/example/webapp/PersonView.java
+++ src/main/java/com/example/webapp/PersonView.java
@@ -5,23 +5,18 @@ import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 
-import org.vaadin.crudui.crud.CrudOperation;
 import org.vaadin.crudui.crud.impl.GridCrud;
-import org.vaadin.crudui.layout.impl.HorizontalSplitCrudLayout;
 
 @Route("")
 @PageTitle("MaxScale Demo")
 public class PersonView extends VerticalLayout {
 
 	public PersonView(PersonService service) {
-		GridCrud<Person> crud = new GridCrud<>(Person.class, new HorizontalSplitCrudLayout());
-		crud.setCrudListener(service);
+		GridCrud<Person> crud = new GridCrud<>(Person.class, service);
 		crud.getGrid().setColumns("name", "creditCardNumber", "writeServerId", "readServerId");
-		crud.getCrudFormFactory().setVisibleProperties(
-				"name", "creditCardNumber", "writeServerId", "readServerId");
-		crud.getCrudFormFactory().setVisibleProperties(CrudOperation.ADD, "name", "creditCardNumber");
-		crud.getCrudFormFactory().setVisibleProperties(CrudOperation.UPDATE, "name", "creditCardNumber");
+		crud.getCrudFormFactory().setVisibleProperties("name", "creditCardNumber");
 		crud.setSizeFull();
+
 		add(new H2("Person CRUD"), crud);
 		setSizeFull();
 	}
